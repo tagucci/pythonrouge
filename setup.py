24fc5aecf7702a176c3704d2568e2f0a50a0f945
tagucci <taguchi.yuya.to0@is.naist.jp>
2016-07-13 18:00:44 +0900

    fix 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="pythonrouge",
-    version="0.0.1",
+    version="0.2",
     description="ROUGE script using python",
     url="http://github.com/tagucci/pythonrouge",
     author="tagucci",
     author_email="taguchi.yuya.to0@is.naist.jp",
-    keywords=["NL", "CL", "natural language processing", "computational linguistics", "summrization"],
+    keywords=["NL", "CL", "natural language processing", "computational linguistics", "summarization"],
     packages=["pythonrouge"],
     package_data={
         'pythonrouge': ['ROUGE-1.5.5/*.*',
