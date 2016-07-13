1e8f7283db87368d4a13dab10928c76a0197cd1b
tagucci <taguchi.yuya.to0@is.naist.jp>
2016-07-13 17:57:42 +0900

    modify to eval R-1, 2, 3, SU4, and L

--- pythonrouge/pythonrouge.py
+++ pythonrouge/pythonrouge.py
@@ -8,7 +8,7 @@ import subprocess
 import sys
 import shutil
 
-def pythonrouge(peer_sentence, model_sentence, ROUGE_path, data_path,  ngram_order=3):
+def pythonrouge(peer_sentence, model_sentence, ROUGE_path, data_path):
     temp_dir = tempfile.mkdtemp()
     summary_dir = os.path.join(temp_dir, "model")
     reference_dir = os.path.join(temp_dir, "peer")
@@ -29,8 +29,6 @@ def pythonrouge(peer_sentence, model_sentence, ROUGE_path, data_path,  ngram_ord
 
     model_sum_list = [abs_model_path]
     ref_sum_list = [[abs_peer_path]]
-
-    options = "-a -m -n " + str(ngram_order)
     
     xml_path = "rouge.xml"
     with open(temp_dir+"/"+xml_path, "w") as xml_file:
@@ -62,39 +60,26 @@ def pythonrouge(peer_sentence, model_sentence, ROUGE_path, data_path,  ngram_ord
         xml_file.close()
     
     abs_xml_path = str(temp_dir+"/"+xml_path)
-    output = subprocess.check_output([ROUGE_path, "-e", data_path, "-a", "-m", "-2", "-4","-n", str(ngram_order), "-x", abs_xml_path], stderr=subprocess.STDOUT)
+    output = subprocess.check_output([ROUGE_path, "-e", data_path, "-a", "-m", "-2", "4","-n", "3", abs_xml_path], stderr=subprocess.STDOUT)
     output = output.decode("utf-8")
     outputs = output.strip().split("\n")
-
-    recall_list = list()
-    precision_list = list()
-    F_measure_list = list()
-
-    for n in range(ngram_order+1):
-        for line in outputs:
-            for score in ["R", "P", "F"]:
-                match = re.findall('X ROUGE-{0} Average_{1}: ([0-9.]+)'.format((n+1), score),line)
-                su_match = re.findall("X ROUGE-S\* Average_{0}: ([0-9.]+)".format(score),line) #su-4
-                if match:
-                    if score == "R":
-                        recall_list.append(float(match[0]))
-                    elif score == "P":
-                        precision_list.append(float(match[0]))
-                    if score == "F":
-                        F_measure_list.append(float(match[0]))
-                if su_match and n == 3:                
-                        if score == "R":
-                            recall_list.append(float(su_match[0]))
-                        elif score == "P":
-                            precision_list.append(float(su_match[0]))
-                        if score == "F":
-                            F_measure_list.append(float(su_match[0]))
-    
-    # you can return precision and recall if you add those list to result
-    result = {}
-    result["rouge-1"] = F_measure_list[0]
-    result["rouge-2"] = F_measure_list[1]
-    result["rouge-3"] = F_measure_list[2]
-    result["rouge-su4"] = F_measure_list[3]
+    F_measure_list = []
+    result = dict()
+    n = 1
+    for line in outputs:
+        rouge = 'ROUGE-{}'.format(n)
+        match    = re.findall('X ROUGE-{} Average_F: ([0-9.]+)'.format(n), line)
+        l_match  = re.findall('X ROUGE-L Average_F: ([0-9.]+)', line)  #ROUGE-L
+        su_match = re.findall('X ROUGE-S4 Average_F: ([0-9.]+)', line)   #ROUGE-SU4
+        if match:
+            F_measure_list.append(float(match[0]))
+            result[rouge] = float(match[0])
+            n += 1
+        elif l_match:
+            F_measure_list.append(float(l_match[0]))    
+            result['ROUGE-L'] = float(l_match[0])
+        elif su_match:                
+            F_measure_list.append(float(su_match[0]))
+            result['ROUGE-SU4'] = float(su_match[0])
     shutil.rmtree(temp_dir)
     return result
